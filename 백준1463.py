aec3066dc9346689c3cc655be2f21b3a217ade70
SangWon <rmlll@naver.com>
2021-01-05 18:33:32 +0900

    백준 파이썬
    10747 1463수정

--- 백준1463.py
+++ 백준1463.py
@@ -1,31 +1,14 @@
-# n = int(input())
-# minCount = 100000
-# def solution(value,cnt):
-#     global minCount
+import sys
 
-#     if value == 1:
-#         minCount = min(minCount,cnt)
-#         return
-    
-#     else:
-#         if value%3 == 0:
-#             solution(value/3,cnt+1)
-#         if value%2 == 0:
-#             solution(value/2,cnt+1)
-#         solution(value-1, cnt+1)
+n = int(sys.stdin.readline())
 
-# solution(n,0)
-# print(minCount)
-
-n = int(input())
-
-nList = [0]*(n+1)
+nList = list(1 for i in range(n+1))
 nList[1] = 0
-nList[2] = 1
-nList[3] = 1
 
 for i in range(4,n+1):
-    if i%3 == 0:
+    if i%6 == 0:
+        nList[i] = min(nList[i//2]+1, nList[i//3]+1)
+    elif i%3 == 0:
         nList[i] = min(nList[i//3]+1, nList[i-1]+1)
     elif i%2 == 0:
         nList[i] = min(nList[i//2]+1, nList[i-1]+1)
